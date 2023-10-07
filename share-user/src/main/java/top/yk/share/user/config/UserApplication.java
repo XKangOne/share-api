f47426fcd46578b7b9997ab3f1dc15759e6df0fd
杨康 <1910297447@qq.com>
2023-10-07 14:11:34 +0800

    修改启动日志，增加 /user-service 上下文，配置 logback 日志

--- share-user/src/main/java/top/yk/share/user/config/UserApplication.java
+++ share-user/src/main/java/top/yk/share/user/config/UserApplication.java
@@ -1,15 +1,22 @@
 package top.yk.share.user.config;
 
 
+import lombok.extern.log4j.Log4j;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @ComponentScan("top.yk")
+@Slf4j
 public class UserApplication {
     public static void main(String[] args) {
-        SpringApplication.run(UserApplication.class,args);
+        SpringApplication app = new SpringApplication(UserApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        log.info("启动成功！!");
+        log.info("测试地址:http://127.0.0.1:{}{}/hello",env.getProperty("server.port"),env.getProperty("server.servlet.context-path"));
     }
-
 }
